d438274122e57187e26f9670887bec735e26d3bb
ZapaTa30 <130095873+ZapaTa30@users.noreply.github.com>
2025-06-05 17:58:53 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/qrmosaicapp/MainActivity.kt
+++ app/src/main/java/com/example/qrmosaicapp/MainActivity.kt
@@ -1,29 +1,11 @@
 package com.example.qrmosaicapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.qrmosaicapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnScan.setOnClickListener {
-            Toast.makeText(this, "Scan Clicked", Toast.LENGTH_SHORT).show()
-        }
-        binding.btnGenerate.setOnClickListener {
-            Toast.makeText(this, "Generate Clicked", Toast.LENGTH_SHORT).show()
-        }
-        binding.btnLogout.setOnClickListener {
-            Toast.makeText(this, "Logout Clicked", Toast.LENGTH_SHORT).show()
-        }
-        binding.btnEncode.setOnClickListener {
-            Toast.makeText(this, "Encode Clicked", Toast.LENGTH_SHORT).show()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
